beef7e36538aec1f8c60bc6a69ffc21ecb1a8961
Hrithic raj <hrithicraj730@gmail.com>
2025-12-02 12:00:33 +0530

    Navbar completed with glassmorphism

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/components/Navbar";
-import bg from '../assets/bg.png';
+import bg from '../assets/Muhsin1.png';
+// import bg from '../assets/hj.jpg';
+
 export default function Home() {
   return (
     <div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='fixed top-4 left-1/2 -translate-x-1/2 z-50 w-3/4
-    backdrop-filter backdrop-blur-xl bg-white/10 bg-opacity-30 border border-white/20
-    px-1/2 py-6 rounded-2xl shadow-xl'>
-        <ul className='flex gap-10 justify-center text-white'>
-            <li>Home</li>
-            <li>About me</li>
-            <li>Portfolio</li>
-            <li>Resume</li>
-            <li>contact</li>
+    <>
+      <nav className="fixed top-4 left-1/2 -translate-x-1/2 z-50 w-3/4 h-[100px] 
+        flex items-center justify-center rounded-3xl bg-[rgba(0,0,0,0.001)] backdrop-filter backdrop-blur-xs bg-opacity-30">
+        <ul className="flex gap-8 text-white font-montserrat text-[16px]">
+          <li className='cursor-pointer hover:text-[#FF6600]'>Home</li>
+          <li className='cursor-pointer hover:text-[#FF6600]'>About me</li>
+          <li className='cursor-pointer hover:text-[#FF6600]'>Portfolio</li>
+          <li className='cursor-pointer hover:text-[#FF6600]'>Resume</li>
+          <li className='cursor-pointer hover:text-[#FF6600]'>Contact</li>
         </ul>
-    </nav>
+      </nav>
+    </>
   )
 }
 
