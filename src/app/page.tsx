7d691791e5462625daf418c342bd3d5fdbf70af4
Hrithic raj <hrithicraj730@gmail.com>
2025-12-02 13:19:51 +0530

    home page background meterials added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,42 @@
 import Navbar from "@/components/Navbar";
-import bg from '../assets/Muhsin1.png';
-// import bg from '../assets/hj.jpg';
+// import bg from '../assets/bg.png';
+import glowLeft from '../assets/glow-left.png';
+import glowRight from '../assets/glow-right.png';
+import linesLeft from '../assets/left1.png';
+import linesRight from '../assets/right1.png';
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <div>
-        <img src={bg.src} alt="" />
+      <div className="relative h-screen bg-[#000000] overflow-hidden">
+
+        {/* Glow Left */}
+        <img 
+          src={glowLeft.src}
+          className="absolute top-0 left-0 object-contain pointer-events-none select-none"
+        />
+
+        {/* Glow Right */}
+        <img 
+          src={glowRight.src}
+          className="absolute top-0 right-0 object-contain pointer-events-none select-none"
+        />
+
+        {/* Lines Left */}
+        <img
+          src={linesLeft.src}
+          className="absolute top-0 left-0 object-contain pointer-events-none select-none mix-blend-overlay opacity-90"
+        />
+
+        {/* Lines Right */}
+        <img
+          src={linesRight.src}
+          className="absolute top-0 right-0 object-contain pointer-events-none select-none mix-blend-overlay opacity-90"
+        />
+
       </div>
-      <div>
-        <img src={bg.src} alt="" />
-      </div>
-      <div>
-        <img src={bg.src} alt="" />
-      </div>
-      <div>
-        <img src={bg.src} alt="" />
-      </div>
-      <div>
-        <img src={bg.src} alt="" />
-      </div>
+
     </div>
   );
 }
