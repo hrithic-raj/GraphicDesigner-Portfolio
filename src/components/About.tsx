8048741092b81b265048cd7a963e24ed52ecaa39
Hrithic raj <hrithicraj730@gmail.com>
2025-12-02 21:04:56 +0530

    Navbar links style updated and home page sptited into defferent components

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 function About() {
   return (
     <div id="about" className="h-screen w-full bg-[#00000] border-b-2"></div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,49 @@
-import React from 'react'
-
 const Navbar = () => {
   return (
-    <>
-      <nav className="fixed top-6 left-1/2 -translate-x-1/2 z-50 w-3/4 h-[100px] 
-        flex items-center justify-center rounded-3xl bg-[rgba(0,0,0,0.001)] backdrop-filter backdrop-blur-sm bg-opacity-30">
-        <ul className="flex gap-10 text-white font-montserrat text-[16px]">
-          <a href="#home">
-          <li className='cursor-pointer hover:text-[#FF6600] font-[montserrat] font-normal'>Home</li>
+    <nav className="
+      fixed top-6 left-1/2 -translate-x-1/2 
+      z-50 w-[75%] h-[90px]
+      flex items-center justify-center
+      rounded-3xl 
+      bg-[rgba(0,0,0,0.001)] backdrop-blur-sm 
+      backdrop-filter bg-opacity-30
+      transition-all duration-300
+    ">
+      <ul className="flex gap-12 text-white font-montserrat text-[16px] font-[montserrat] font-normal">
+        
+        <li>
+          <a href="#home" className="nav-link">
+            Home
           </a>
-          <a href="#about">
-          <li className='cursor-pointer hover:text-[#FF6600] font-[montserrat] font-normal'>About me</li>
-          </a>
-          <a href="#portfolio">
-          <li className='cursor-pointer hover:text-[#FF6600] font-[montserrat] font-normal'>Portfolio</li>
-          </a>
-          <li className='cursor-pointer hover:text-[#FF6600] font-[montserrat] font-normal'>Resume</li>
-          <li className='cursor-pointer hover:text-[#FF6600] font-[montserrat] font-normal'>Contact</li>
-        </ul>
-      </nav>
-    </>
-  )
-}
+        </li>
 
-export default Navbar
+        <li>
+          <a href="#about" className="nav-link">
+            About me
+          </a>
+        </li>
+
+        <li>
+          <a href="#portfolio" className="nav-link">
+            Portfolio
+          </a>
+        </li>
+
+        <li>
+          <a href="#resume" className="nav-link">
+            Resume
+          </a>
+        </li>
+
+        <li>
+          <a href="#contact" className="nav-link">
+            Contact
+          </a>
+        </li>
+
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
