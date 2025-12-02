9c71eac7f82017db3ea4a6daf8e0e1955cce5dfb
Hrithic raj <hrithicraj730@gmail.com>
2025-12-02 17:21:18 +0530

    text Overlay image added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 // import bg from '../assets/bg.png';
-import bg from '../assets/bgfinal.jpg';
+import bg from '../assets/To site 1.jpg';
+import cutPhoto from '../assets/To site cut.png';
 import glowLeft from '../assets/glow-left.png';
 import glowRight from '../assets/glow-right.png';
 import linesLeft from '../assets/left1.png';
@@ -16,7 +17,7 @@ export default function Home() {
         {/* Background image behind everything */}
         <img
           src={bg.src}
-          className="w-full h-full object-cover pointer-events-none select-none z-0"
+          className="w-full h-full -translate-y-10 object-cover pointer-events-none select-none z-0"
         />
         {/* Glow Left */}
         <img 
@@ -45,19 +46,19 @@ export default function Home() {
         {/* Pressure Text */}
         {/* <div className="absolute top-1/3 translate-x-16 -translate-y-1/2 z-30 w-[75%]"> */}
         <div className="absolute top-1/3 left-1/2 
-          -translate-x-1/2 -translate-y-1/2 
+          -translate-x-1/2 -translate-y-1/2
           w-[70%]
           h-[200px]
           flex justify-center items-center
           z-3"
         >
-          <TextPressure className="w-full"/>
+          <TextPressure className="w-full z-3"/>
         </div>
 
         {/* Text Top image */}
-        {/* <img src="" alt="" /> */}
+        <img src={cutPhoto.src} className="absolute object-cover top-0 -translate-y-10 z-5 w-full h-full" alt="" />
 
-        <div className="absolute bottom-0 left-1/8 bg-[#0F0F0F] w-3/4 h-14">
+        <div className="absolute bottom-0 left-1/8 bg-[#0F0F0F] w-3/4 h-14 z-10">
 
         </div>
 
