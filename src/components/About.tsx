403ba5c980ded7ad9c052ab22c8ee17dcbaff3ac
Hrithic raj <hrithicraj730@gmail.com>
2025-12-04 02:35:33 +0530

    About resposiveness fixed and portfolio section started

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,12 @@
 import aboutPhoto from '../assets/about.png';
 function About() {
   return (
-    <div id="about" className="h-screen w-full bg-[#00000] border-b-2">
+    <div id="about" className="h-screen w-full bg-[#00000]">
       <div className="flex justify-center items-center h-full w-full">
         {/* About Box */}
-        <div className="flex justify-between h-1/2 w-[60%]">
+        <div className="flex justify-between items-center min-h-1/2 h-[60%] w-[70%]">
           {/* Photo */}
-          <div className="flex justify-center items-center w-[40%] h-full">
+          <div className="flex justify-center items-center w-[35%] h-full">
             <img 
               src={aboutPhoto.src} 
               alt=""
@@ -14,14 +14,11 @@ function About() {
             />
           </div>
           {/* Info */}
-          <div className="flex justify-center items-center w-[60%] h-full">
-            <div className='flex flex-col justify-between w-[90%] h-[60%]'>
+          <div className="flex justify-start items-center w-[60%] h-[90%]">
+            <div className='flex flex-col justify-center gap-5 w-[90%] h-[70%]'>
               <span className='font-[montserrat] font-bold text-[20px] leading-5 tracking-[0.02em] text-[#FF6600]'>About Me</span>
-              {/* <p>I’m a passionate Graphic Designer and Video Editor who thrives on turning ideas into powerful visuals and compelling stories. With a keen eye for detail and a love for aesthetics, I craft designs and edits that connect emotionally and communicate clearly. From branding and motion graphics to cinematic edits, I focus on blending creativity with strategy to bring each project to life. I believe every frame, color, and composition should serve a purpose to inspire, engage, and leave a lasting impression</p> */}
               <p className="
-                font-montserrat font-normal
-                text-[16px] leading-5
-                tracking-[0.02em]
+                font-[montserrat] font-light min-text-sm
                 text-white
               ">
                 I’m a passionate Graphic Designer and Video Editor who thrives on turning ideas into powerful 
@@ -31,17 +28,19 @@ function About() {
                 to life. I believe every frame, color, and composition should serve a purpose to inspire, engage, 
                 and leave a lasting impression.
               </p>
-              <div className='flex justify-between mt-5 h-[20%] gap-5'>
-                <button className='bg-[#FF6600] w-1/2 h-full rounded-[36px] cursor-pointer text-[34px] hover:text-[30px]'>
+
+              {/* buttons */}
+              <div className='flex justify-between h-[20%] gap-5'>
+                <button className='flex justify-center items-center bg-[#FF6600] w-1/2 h-full rounded-[36px] cursor-pointer text-[34px] hover:text-[30px]'>
                   <span
-                    className='font-[montserrat] font-normal leading-[27px] text-center tracking-[0.02em] text-white'
+                    className='font-[montserrat] font-normal text-center tracking-[0.02em] text-white'
                   >
                     Resume
                   </span>
                 </button>
-                <button className='border border-[#FF6600] w-1/2 h-full rounded-[36px] cursor-pointer text-[34px] hover:text-[30px]'>
+                <button className='flex justify-center items-center border border-[#FF6600] w-1/2 h-full rounded-[36px] cursor-pointer text-[34px] hover:text-[30px]'>
                   <span
-                    className='font-[montserrat] font-normal leading-[27px] text-center tracking-[0.02em] text-white'
+                    className='font-[montserrat] font-normal text-center tracking-[0.02em] text-white'
                   >
                     Hire me
                   </span>
--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,6 +1,24 @@
+'use client'
+import glowMiddle from '../assets/glow-last-middle.png';
 function Portfolio() {
   return (
-    <div id="portfolio" className="h-screen w-full bg-[#00000] border-b-2"></div>
+    <div id="portfolio" className="relative h-screen w-full bg-[#00000] border-b-2">
+      <img 
+        src={glowMiddle.src}
+        draggable={false}
+        onDragStart={(e) => e.preventDefault()}
+        onMouseDown={(e) => e.preventDefault()}
+        className="absolute top-[10%] left-1/3 object-contain pointer-events-none select-none z-1"
+      />
+      <div className="flex justify-center items-center h-full w-full">
+        <div className="grid grid-cols-2 grid-rows-2 h-[60%] w-3/4 gap-5">
+          <div className="border w-full h-full rounded-4xl"></div>
+          <div className="border w-full h-full rounded-4xl"></div>
+          <div className="border w-full h-full rounded-4xl"></div>
+          <div className="border w-full h-full rounded-4xl"></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
