4883ef13e12ebe54bc8dfbf3576a7b29711693eb
Hrithic raj <hrithicraj730@gmail.com>
2025-12-05 01:57:17 +0530

    footer text and image dragging and selecting removed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+'use client'
 import icon1 from '../assets/footer-icon1.png'
 import icon2 from '../assets/footer-icon2.png'
 import icon3 from '../assets/footer-icon3.png'
@@ -8,13 +9,48 @@ function Footer() {
     <div className='w-full h-[130px] flex flex-col items-center'>
         <div className='border-t border-[#202020] w-3/4 h-1'></div>
         <div className='flex justify-between w-3/4 h-full items-center pb-[30px]'>
-            <span className='font-[CabinetGrotesk] font-normal text-[20px] leading-[112.5%] text-[#FF6600]'>Muhamed Muhsin P</span>
-            <div className='flex justify-between gap-5 w-[20%] h-[20%]'>
-                <img src={icon1.src} alt="" className='cursor-pointer'/>
-                <img src={icon2.src} alt="" className='cursor-pointer'/>
-                <img src={icon3.src} alt="" className='cursor-pointer'/>
-                <img src={icon4.src} alt="" className='cursor-pointer'/>
-                <img src={icon5.src} alt="" className='cursor-pointer'/>
+            <span className='font-[CabinetGrotesk] font-normal text-[20px] leading-[112.5%] text-[#FF6600] select-none'>Muhamed Muhsin P</span>
+            <div className='flex justify-between gap-5 w-[20%] h-[20%] select-none'>
+                <img 
+                    src={icon1.src} 
+                    alt=""
+                    draggable={false}
+                    onDragStart={(e) => e.preventDefault()}
+                    onMouseDown={(e) => e.preventDefault()}
+                    className='cursor-pointer'
+                />
+                <img 
+                    src={icon2.src} 
+                    alt="" 
+                    draggable={false}
+                    onDragStart={(e) => e.preventDefault()}
+                    onMouseDown={(e) => e.preventDefault()}
+                    className='cursor-pointer'
+                />
+                <img 
+                    src={icon3.src} 
+                    alt="" 
+                    draggable={false}
+                    onDragStart={(e) => e.preventDefault()}
+                    onMouseDown={(e) => e.preventDefault()}
+                    className='cursor-pointer'
+                />
+                <img 
+                    src={icon4.src} 
+                    alt="" 
+                    draggable={false}
+                    onDragStart={(e) => e.preventDefault()}
+                    onMouseDown={(e) => e.preventDefault()}
+                    className='cursor-pointer'
+                />
+                <img 
+                    src={icon5.src} 
+                    alt="" 
+                    draggable={false}
+                    onDragStart={(e) => e.preventDefault()}
+                    onMouseDown={(e) => e.preventDefault()}
+                    className='cursor-pointer'
+                />
             </div>
         </div>
     </div>
